ee09a4ca22226c017ad50516f2245f8f7c5fc37e
wangf <wf_chn@163.com>
2013-07-01 22:49:01 +0800

    icon error proc for

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  errors = Array(instance.error_message).join(',')
+   unless html_tag =~ /^<label/
+    %(<div class="field_with_errors">#{html_tag}<span class="help-inline">&nbsp;#{errors}</span></div>).html_safe
+    else
+    %(<div class="field_with_errors">#{html_tag}</div>).html_safe
+      
+  end
+end
